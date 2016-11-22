a77b0fd8ff962d1ddf0e57b7ed4aa69da1bdec3c
root <axetransit@gmail.com>
2016-11-22 15:35:46 +0000

    Remove Old Chat rooms

--- include/nav.php
+++ include/nav.php
@@ -94,10 +94,10 @@ EOT;
 
 		$nav['usermenu'][] = Array('photos/' . $channel['channel_address'], t('Photos'), "", t('Your photos'),'photos_nav_btn');
 		$nav['usermenu'][] = Array('cloud/' . $channel['channel_address'],t('Files'),"",t('Your files'),'cloud_nav_btn');
-
+/**
 		if((! $basic) && feature_enabled(local_channel(),'ajaxchat'))
 			$nav['usermenu'][] = Array('chat/' . $channel['channel_address'], t('Chat'),"",t('Your chatrooms'),'chat_nav_btn');
-
+**/
 
 		require_once('include/menu.php');
 		$has_bookmarks = menu_list_count(local_channel(),'',MENU_BOOKMARK) + menu_list_count(local_channel(),'',MENU_SYSTEM|MENU_BOOKMARK);
